1ebaa6025d27bd3d77af1bcc91ca7e7704b49d7f
Neil Miller <neilsmiller95@gmail.com>
2020-12-02 19:43:48 -0500

    Rename to app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+# (sys.version)
+print(sys.executable)
+
+print("Neil")
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
